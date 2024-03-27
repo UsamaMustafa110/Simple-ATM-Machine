16414c08807695cf785d28ca87edcd4c19b52a0d
USAMA MUSTAFA <hu95130@gmail.com>
2024-03-27 23:34:35 +0500

    little changes

--- index.js
+++ index.js
@@ -11,53 +11,78 @@ let pinAnswer = await inquirer.prompt([
 ]);
 if (pinAnswer.userPin == myPin) {
     console.log("Pin is correct");
+    console.log("Welcome Usama Mustafa");
+    let accountType = await inquirer.prompt([
+        {
+            name: "accountType",
+            type: "list",
+            choices: ["current", "saving"],
+            message: "Enter your account type",
+        },
+    ]);
+    if (accountType.accountType == "current") {
+        console.log("your account is current");
+    }
+    else if (accountType.accountType == "saving") {
+        console.log("your account is saving");
+    }
+    let operation = await inquirer.prompt([
+        {
+            name: "operation",
+            type: "list",
+            choices: ["withdrawal of amount", "balance inquiry"],
+            message: "Enter your operation",
+        },
+    ]);
+    if (operation.operation == "withdrawal of amount") {
+        let amount = await inquirer.prompt([
+            {
+                name: "amount",
+                type: "list",
+                choices: [1000, 2000, 5000, 10000, "or-any-other-amount"],
+                message: "Enter your amount",
+            },
+        ]);
+        // if (amount.amount == 1000) {
+        //   console.log("transaction successful");
+        //   console.log("your remaining balance is", myBalance - amount.amount);
+        // } else if (amount.amount == 2000) {
+        //   console.log("transaction successful");
+        //   console.log("your remaining balance is", myBalance - amount.amount);
+        // } else if (amount.amount == 5000) {
+        //   console.log("transaction successful");
+        //   console.log("your remaining balance is", myBalance - amount.amount);
+        // } else if (amount.amount == 10000) {
+        //   console.log("transaction successful");
+        //   console.log("your remaining balance is", myBalance - amount.amount);
+        // }
+        if (amount.amount == "or-any-other-amount") {
+            let anotherAmount = await inquirer.prompt([
+                {
+                    name: "anotherAmount",
+                    type: "number",
+                    message: "Enter your amount",
+                },
+            ]);
+            if (anotherAmount.anotherAmount <= myBalance) {
+                console.log("transaction successful");
+                console.log("your remaining balance is", myBalance - anotherAmount.anotherAmount);
+            }
+            else if (anotherAmount.anotherAmount > myBalance) {
+                console.log("transaction failed");
+                console.log("insufficient balance");
+            }
+        }
+        else {
+            console.log("transaction successful");
+            console.log("your remaining balance is", myBalance - amount.amount);
+        }
+    }
+    if (operation.operation == "balance inquiry") {
+        console.log("your balance is", myBalance);
+    }
+    console.log("Thank you for using my ATM, have a nice day");
 }
 else {
     console.log("incorrect pin, try again");
 }
-if (pinAnswer.userPin == myPin) {
-    console.log("Welcome Usama Mustafa");
-}
-let accountType = await inquirer.prompt([
-    {
-        name: "accountType",
-        type: "list",
-        choices: ["current", "saving"],
-        message: "Enter your account type",
-    },
-]);
-if (accountType.accountType == "current") {
-    console.log("your account is current");
-}
-else if (accountType.accountType == "saving") {
-    console.log("your account is saving");
-}
-let operation = await inquirer.prompt([
-    {
-        name: "operation",
-        type: "list",
-        choices: ["withdrawal of amount", "balance inquiry"],
-        message: "Enter your operation",
-    },
-]);
-if (operation.operation == "withdrawal of amount") {
-    let amount = await inquirer.prompt([
-        {
-            name: "amount",
-            type: "number",
-            message: "Enter your amount",
-        },
-    ]);
-    if (amount.amount <= myBalance) {
-        myBalance = myBalance - amount.amount;
-        console.log("transaction successful");
-        console.log("your remaining balance is", myBalance);
-    }
-    else {
-        console.log("insufficient balance");
-    }
-}
-if (operation.operation == "balance inquiry") {
-    console.log("your balance is", myBalance);
-}
-console.log("Thank you for using my ATM, have a nice day");
--- index.ts
+++ index.ts
@@ -12,12 +12,7 @@ let pinAnswer = await inquirer.prompt([
 ]);
 if (pinAnswer.userPin == myPin) {
   console.log("Pin is correct");
-} else {
-  console.log("incorrect pin, try again");
-}
-if (pinAnswer.userPin == myPin) {
   console.log("Welcome Usama Mustafa");
-}
 let accountType = await inquirer.prompt([
   {
     name: "accountType",
@@ -28,9 +23,7 @@ let accountType = await inquirer.prompt([
 ]);
 if (accountType.accountType == "current") {
   console.log("your account is current");
-}
-else if (accountType.accountType == "saving")
-{
+} else if (accountType.accountType == "saving") {
   console.log("your account is saving");
 }
 let operation = await inquirer.prompt([
@@ -45,21 +38,50 @@ if (operation.operation == "withdrawal of amount") {
   let amount = await inquirer.prompt([
     {
       name: "amount",
-      type: "number",
+      type: "list",
+      choices: [1000, 2000, 5000, 10000, "or-any-other-amount"],
       message: "Enter your amount",
     },
   ]);
-  if (amount.amount <= myBalance) {
-    myBalance = myBalance - amount.amount;
-    console.log("transaction successful");
-    console.log("your remaining balance is", myBalance);
-  } else {
-    console.log("insufficient balance");
+
+  // if (amount.amount == 1000) {
+  //   console.log("transaction successful");
+  //   console.log("your remaining balance is", myBalance - amount.amount);
+  // } else if (amount.amount == 2000) {
+  //   console.log("transaction successful");
+  //   console.log("your remaining balance is", myBalance - amount.amount);
+  // } else if (amount.amount == 5000) {
+  //   console.log("transaction successful");
+  //   console.log("your remaining balance is", myBalance - amount.amount);
+  // } else if (amount.amount == 10000) {
+  //   console.log("transaction successful");
+  //   console.log("your remaining balance is", myBalance - amount.amount);
+  // }
+  if (amount.amount == "or-any-other-amount") {
+    let anotherAmount = await inquirer.prompt([
+      {
+        name: "anotherAmount",
+        type: "number",
+        message: "Enter your amount",
+      },
+  ]);
+  if (anotherAmount.anotherAmount <= myBalance) {
+  console.log("transaction successful");
+  console.log("your remaining balance is", myBalance - anotherAmount.anotherAmount);
+} else if (anotherAmount.anotherAmount > myBalance) {
+  console.log("transaction failed");
+  console.log("insufficient balance");
   }
 }
-
-if (operation.operation == "balance inquiry") {
-  console.log("your balance is", myBalance);
+else {
+  console.log("transaction successful");
+    console.log("your remaining balance is", myBalance - amount.amount);
+  }
 }
-
-console.log("Thank you for using my ATM, have a nice day");
+  if (operation.operation == "balance inquiry") {
+    console.log("your balance is", myBalance);
+  }
+  console.log("Thank you for using my ATM, have a nice day");
+} else {
+  console.log("incorrect pin, try again");
+}
